04b3f14e22e493c3ebd738de0d6de79115ab9a2c
hajar <hajardakkouki@gmail.com>
2026-03-30 12:00:51 +0100

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Devops-doker ci pipline!"
+    return "Hello, Devops-doker cifffffffffffffff pipline!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     
--- deploy.ps1
+++ deploy.ps1
@@ -2,8 +2,8 @@ param(
     [string]$DockerUser,
     [string]$DockerToken
 )
-docker login -u $DockerUser -p $DockerToken
+docker login -u hajardk -p $DockerToken
 docker stop flask_app -ErrorAction SilentlyContinue
 docker rm flask_app -ErrorAction SilentlyContinue
-docker pull $DockerUser/flask_devops_demo:latest
-docker run -d  --name flask_app -p 5000:5000 $DockerUser/flask_devops_demo:latest
+docker pull hajardk/flask_devops_demo:latest
+docker run -d  --name flask_app -p 5000:5000 hajardk/flask_devops_demo:latest
